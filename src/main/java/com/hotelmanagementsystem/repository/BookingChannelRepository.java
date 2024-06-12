17fae397e6daee691bfead68b39acc886fbfdabc
Aditya Kalambe <adityakalambe20@gmail.com>
2024-06-12 13:35:27 +0530

    Repositories

---
+++ src/main/java/com/hotelmanagementsystem/repository/BookingChannelRepository.java
@@ -0,0 +1,9 @@
+package com.hotelmanagementsystem.repository;
+
+import com.hotelmanagementsystem.entity.BookingChannel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingChannelRepository extends JpaRepository<Long, BookingChannel> {
+}
