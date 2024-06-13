272223f231a2e191d261604dd97b9697cfa102c8
Aditya Kalambe <adityakalambe20@gmail.com>
2024-06-13 17:44:34 +0530

    Repositories of entities

--- src/main/java/com/hotelmanagementsystem/repository/BookingChannelRepository.java
+++ src/main/java/com/hotelmanagementsystem/repository/BookingChannelRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookingChannelRepository extends JpaRepository<Long, BookingChannel> {
+public interface BookingChannelRepository extends JpaRepository<BookingChannel, Long> {
 }
--- src/main/java/com/hotelmanagementsystem/repository/ServiceRequestRepository.java
+++ src/main/java/com/hotelmanagementsystem/repository/ServiceRequestRepository.java
@@ -1,8 +1,9 @@
 package com.hotelmanagementsystem.repository;
 
+import com.hotelmanagementsystem.entity.ServiceRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ServiceRequestRepository extends JpaRepository<ServiceRequestRepository, Long> {
+public interface ServiceRequestRepository extends JpaRepository<ServiceRequest, Long> {
 }
